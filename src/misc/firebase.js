5a19a89a7f3c827dbaf227bac276cde45a44bacb
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-26 20:16:01 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyClZzJu0rt4JW7BTvzsVLuanE3uYBGCljM",
+    authDomain: "chat-web-app-794e2.firebaseapp.com",
+    projectId: "chat-web-app-794e2",
+    storageBucket: "chat-web-app-794e2.appspot.com",
+    messagingSenderId: "588256379917",
+    appId: "1:588256379917:web:c6bb656be528a5f1644144"
+  };
+
+
+const app= firebase.initializeApp(config);
