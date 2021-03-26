0ccc5d73ebc802e9127351d783986611be974214
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-27 02:13:37 +0530

    added the icons and buttons

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Container, Grid, Panel, Row,Col, Button, Icon } from 'rsuite'
 
 const SignIn = () => {
     return (
-        <div>
-            Sign In
-        </div>
+        <Container>
+            <Grid>
+                <Row>
+                    <Col xs={24} md={12} mdOffset={6}>
+                        <Panel>
+                            <div className="text-center">
+                                <h2>Welcome to Chat</h2>
+                                <p>Progressive chat app for neopyhtes</p>
+                            </div>
+
+
+                            <div>
+                                <Button block color="blue">
+                                    <Icon icon="facebook" /> Continue with Facebook
+                                </Button>
+
+                                <Button block color="green">
+                                    <Icon icon="Google" /> Continue with Google
+                                </Button>
+                            </div>
+                        </Panel>
+                    </Col>
+                </Row>
+            </Grid>
+        </Container>
     )
 }
 
