568328e9733030ed063783b45883fd88cf6a0f83
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-28 03:11:17 +0530

    managed realtime

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,13 +1,55 @@
-import React,{createContext,useState,useContext} from 'react'
+import React,{createContext,useState,useContext,useEffect} from 'react'
+import { auth, database } from '../misc/firebase';
 
 
 const ProfileContext= createContext();
 
 export const ProfileProvider = ({children}) => {
-    const [profile] = useState(false);
+    const [profile,setProfile] = useState(null);
+    const [isloading, setIsLoading] =useState(true);
+
+    useEffect(()=>{
+        let userRef;
+
+        const authUnsub =auth.onAuthStateChanged(authObj => {
+            
+            if(authObj){
+                userRef =database.ref(`/profiles/${authObj.uid}`);
+                userRef.on('value',(snap)=>{
+                    const {name,createdAt}=snap.val();
+
+                    const data= {
+                        name,
+                        createdAt,
+                        uid: authObj.uid,
+                        email:authObj.email
+                    }
+                setProfile(data);
+                setIsLoading(false);
+                });
+            }
+            else{
+
+                if(userRef){
+                    userRef.off();
+                }
+                setProfile(null);
+                setIsLoading(false);
+            }
+
+        });
+        return ()=>{
+
+            if(userRef){
+                userRef.off();
+            }
+            authUnsub();
+        }
+
+    },[]);
 
     return( 
-    <ProfileContext.Provider value={profile}>
+    <ProfileContext.Provider value={{isloading,profile}}>
         {children}
     </ProfileContext.Provider>
     );
