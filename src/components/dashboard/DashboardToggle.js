823400924eb2093fdef42503d20528912b40b6f6
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-29 00:44:19 +0530

    started building the sidebar

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Button, Icon } from 'rsuite'
+import { Button, Drawer, Icon } from 'rsuite'
+import Dashboard from '.';
+import { useModalState } from '../../misc/custom-hooks'
 
 const DashboardToggle = () => {
+
+
+    const {isOpen, close, open} =useModalState ();
     return (
         <>
-          <Button block color="blue">
+          <Button block color="blue" onClick={open}>
               <Icon icon="dashboard"/> Dashboard
           </Button>  
+
+          <Drawer show={isOpen} onHide={close} placement="left">
+            <Dashboard />
+          </Drawer>
         </>
     )
 }
