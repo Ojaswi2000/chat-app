08b5de4e2e20e370a824349629982e883cce97bc
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-04-05 19:48:40 +0530

    conditionally rendered msgs

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -2,10 +2,13 @@ import React,{useState,useEffect} from 'react'
 import { useParams } from 'react-router';
 import { database } from '../../../misc/firebase';
 import { tranformToArrayWithId } from '../../../misc/helpers';
+import MessageItem from './MessageItem';
 
 const Messages = () => {
     const {chatId} = useParams();
     const [messages,setMessages]= useState(null);
+    const isChatEmpty = messages && messages.length===0;
+    const canShowMessages= messages && messages.length>0;
 
     useEffect(() => {
         const messagesRef= database.ref('/messages');
@@ -21,9 +24,11 @@ const Messages = () => {
         
     }, [chatId])
     return (
-        <div>
-            messages
-        </div>
+        <ul className="msg-list custom-scroll">
+            {isChatEmpty && <li>No messages yet...</li>}
+            {canShowMessages && messages.map(msg=> <MessageItem  key={msg.id} messages={msg} />)}
+            
+        </ul>
     )
 }
 
