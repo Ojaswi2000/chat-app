5ee2b867d0b33bc997025bf2d87b36405add61b0
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-04-05 19:39:50 +0530

    added realtime listener

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase';
+import { tranformToArrayWithId } from '../../../misc/helpers';
 
 const Messages = () => {
+    const {chatId} = useParams();
+    const [messages,setMessages]= useState(null);
+
+    useEffect(() => {
+        const messagesRef= database.ref('/messages');
+        
+        messagesRef.orderByChild('roomId').equalTo(chatId).on('value',(snap)=>{
+            const data= tranformToArrayWithId(snap.val());
+            setMessages(data);
+        })
+        
+        return ()=>{
+            messagesRef.off('value');
+        }
+        
+    }, [chatId])
     return (
         <div>
             messages
