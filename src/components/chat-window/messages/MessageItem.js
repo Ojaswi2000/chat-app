6bcf105f16b31c134822386b567dbc2827627a96
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-04-05 20:27:57 +0530

    displayed modal in chat

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProfileAvatar from '../../ProfileAvatar';
 import TimeAgo from 'timeago-react'; 
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 const MessageItem = ({message}) => {
     const {author,createdAt,text} = message;
@@ -9,6 +10,7 @@ const MessageItem = ({message}) => {
             <div className="d-flex align-items-center font-bolder mb-1">
                 <ProfileAvatar src={author.avatar} name={author.name} className="ml-1" size="s" />
                 <span className="ml-2">{author.name} </span>
+                <ProfileInfoBtnModal profile={author}/>
                 <TimeAgo
                     datetime={createdAt} className="font-normal text-black-45 ml-2"
                 />
