17a4fec190f858f1844b7105bcc732e3177cf3d5
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-04-06 19:59:55 +0530

    changes rules and handled  realtime presence

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,5 +1,16 @@
 import React,{createContext,useState,useContext,useEffect} from 'react'
 import { auth, database } from '../misc/firebase';
+import firebase from 'firebase/app'
+
+const isOfflineForDatabase = {
+    state: 'offline',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+    state: 'online',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 
 const ProfileContext= createContext();
@@ -10,10 +21,12 @@ export const ProfileProvider = ({children}) => {
 
     useEffect(()=>{
         let userRef;
-
+        let userStatusRef;
+        
         const authUnsub =auth.onAuthStateChanged(authObj => {
             
             if(authObj){
+                userStatusRef = database.ref(`/status/${authObj.uid}`);
                 userRef =database.ref(`/profiles/${authObj.uid}`);
                 userRef.on('value',(snap)=>{
                     const {name,createdAt,avatar}=snap.val();
@@ -28,11 +41,26 @@ export const ProfileProvider = ({children}) => {
                 setProfile(data);
                 setIsLoading(false);
                 });
+
+
+                database.ref('.info/connected').on('value',(snapshot)=> {
+                    // If we're not currently connected, don't do anything.
+                    if (snapshot.val() === false) {
+                        return;
+                    };
+                    userStatusRef.onDisconnect().set(isOfflineForDatabase).then(() => {
+                       
+                        userStatusRef.set(isOnlineForDatabase);
+                    });
+                });
             }
             else{
 
                 if(userRef){
                     userRef.off();
+                }
+                if(userStatusRef){
+                    userStatusRef.off();
                 }
                 setProfile(null);
                 setIsLoading(false);
@@ -43,6 +71,9 @@ export const ProfileProvider = ({children}) => {
             authUnsub();
             if(userRef){
                 userRef.off();
+            }
+            if(userStatusRef){
+                userStatusRef.off();
             }
         }
 
