55c1160589a7b16b009171f700183e05eb2b9d1a
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-04-07 20:07:14 +0530

    added tooltip

--- src/components/PresenceDot.js
+++ src/components/PresenceDot.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { usePresence } from '../misc/custom-hooks'
+import {Badge, Whisper} from 'rsuite'
 
 const PresenceDot = ({uid}) => {
     const presence = usePresence(uid);
+    
     return (
-        <div>
-            
-        </div>
+        <Whisper placement="top" trigger="hover" speaker={
+            <Tooltip>
+              This is a help <i>tooltip</i> .
+            </Tooltip>
+        }>
+            <Badge />
+        </Whisper>
     )
 }
 
