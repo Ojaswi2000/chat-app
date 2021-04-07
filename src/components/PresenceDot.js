a69d9340a170a7e6d4d41f7c0b148863e3c869a9
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-04-07 20:15:51 +0530

    added gettext and getcolor

--- src/components/PresenceDot.js
+++ src/components/PresenceDot.js
@@ -2,16 +2,39 @@ import React from 'react'
 import { usePresence } from '../misc/custom-hooks'
 import {Badge, Whisper} from 'rsuite'
 
+const getColor =(presence) =>{
+    if(!presence){
+        return "gray";
+    }
+    switch(presence.state){
+        case 'online':
+            return "green"
+        case "offline":
+            return "red"
+        default:
+            return "gray"
+    }
+}
+
+const getText= (presence) =>{
+    if(!presence){
+        return "Unknown state";
+    }
+    return presence.state === 'online'? 
+    "Online" : 
+    `Last online ${new Date(presence.last_changed).toLocaleDateString()}`;
+}
+
 const PresenceDot = ({uid}) => {
     const presence = usePresence(uid);
     
     return (
         <Whisper placement="top" trigger="hover" speaker={
             <Tooltip>
-              This is a help <i>tooltip</i> .
+              {getText(presence)}
             </Tooltip>
         }>
-            <Badge />
+            <Badge className="cursor-pointer" style={{backgroundColor:getColor(presence)}} />
         </Whisper>
     )
 }
