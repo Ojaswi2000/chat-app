d8b47f5a1aa44d2b4d7d7a3ed4d09270a4a9c46d
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-04-09 01:33:02 +0530

    added all security and grant permission rules

--- src/components/chat-window/messages/ProfileInfoBtnModal.js
+++ src/components/chat-window/messages/ProfileInfoBtnModal.js
@@ -3,7 +3,7 @@ import { Button, Modal } from 'rsuite'
 import { useModalState } from '../../../misc/custom-hooks';
 import ProfileAvatar from '../../ProfileAvatar';
 
-const ProfileInfoBtnModal = ({profile,...btnProps}) => {
+const ProfileInfoBtnModal = ({profile,children,...btnProps}) => {
     const shortName= profile.name.split(' ')[0];
     const {isOpen,open,close} =useModalState();
     const {name,avatar,createdAt} =profile;
@@ -27,6 +27,7 @@ const ProfileInfoBtnModal = ({profile,...btnProps}) => {
             <p>Member since {memberSince} </p>
             </Modal.Body>
             <Modal.Footer>
+                {children}
             <Button block onClick={close} color="orange">
             Close
             </Button>
