777730850d310489036c9b44e0bbc9e5c5d16fe2
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-04-14 00:09:59 +0530

    uploaded files to storage

--- src/components/chat-window/bottom/AttachmentModalBtn.js
+++ src/components/chat-window/bottom/AttachmentModalBtn.js
@@ -1,15 +1,47 @@
 import React,{useState} from 'react'
-import { Button, Icon, InputGroup, Modal, Uploader } from 'rsuite'
+import { useParams } from 'react-router'
+import { Alert, Button, Icon, InputGroup, Modal, Uploader } from 'rsuite'
 import {useModalState} from '../../../misc/custom-hooks'
+import { storage } from '../../../misc/firebase'
 
 const MAX_FILE_SIZE = 1000*1024*5
 
-const AttachmentModalBtn = () => {
+const AttachmentModalBtn = ({afterUpload}) => {
     const {isOpen,open,close} = useModalState();
     const [fileList,setFileList]= useState([]);
+    const {chatId}=useParams();
+    const [isLoading,setIsLoading]= useState(false);
     const onChange=(fileArr)=>{
         const filtered= fileArr.filter(el => el.blobFile.size <= MAX_FILE_SIZE).slice(0,5);
         setFileList(filtered);
+    }
+    const onUpload=async()=>{
+        try {
+            const uploadPromises= fileList.map(f => {
+                return storage.ref(`/chat/${chatId}`)
+                .child(Date.now() + f.name)
+                .put(f.blobFile, {cacheControl:`public,max-age-${3600*24*3}`});
+            })
+            const uploadSnapshots= await Promise.all(uploadPromises);
+            const shapePromises=uploadSnapshots.map(async snap=>{
+                return {
+                    contentType: snap.metadata.contentType,
+                    name:snap.metadata.name,
+                    url: await snap.ref.getDownloadURL()
+                }
+            })
+
+            const files= await Promise.all(shapePromises);
+            await afterUpload(files);
+            setIsLoading(false);
+            close();
+            
+        } catch (err) {
+            setIsLoading(false);
+            Alert.error(err.message,4000);
+            
+        }
+
     }
     return (
         <>
@@ -32,10 +64,11 @@ const AttachmentModalBtn = () => {
                 multiple
                 listType="picture-text"
                 className="w-100"
+                disabled={isLoading}
                 />
                 </Modal.Body>
                 <Modal.Footer>
-                <Button block>
+                <Button block onClick={onUpload} disabled={isLoading}>
                 Send to chat
                 </Button>
                 <div className="text-right mt-2">
